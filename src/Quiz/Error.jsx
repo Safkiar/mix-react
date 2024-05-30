ff1964035226a812265cdae102378aa581749651
Safkiar <m.kikowski97@gmail.com>
2024-05-30 22:36:20 +0200

    new build

--- src/Quiz/Error.jsx
+++ src/Quiz/Error.jsx
@@ -1,7 +1,7 @@
 function Error({ error }) {
   return (
     <p className={error}>
-      <span>💥</span> There was an error fecthing questions.
+      <span>💥</span> There was an error fetching questions.
     </p>
   );
 }
--- src/Quiz/Quiz.jsx
+++ src/Quiz/Quiz.jsx
@@ -89,7 +89,7 @@ function Quiz() {
   ] = useReducer(reducer, initialState);
 
   useEffect(function () {
-    fetch("../../data/questions.json")
+    fetch("../../public/data/questions.json")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
